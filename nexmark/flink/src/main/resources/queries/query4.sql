694b2e7f58ff292b949361e2f4aabba5105eb345
Carlo <krevs3600@gmail.com>
2025-01-19 16:00:31 +0100

    fixed query

--- nexmark/flink/src/main/resources/queries/query4.sql
+++ nexmark/flink/src/main/resources/queries/query4.sql
@@ -1,11 +1,15 @@
 SELECT
-    Q.category,
-    AVG(Q.final),
+    Q.category_id,
+    AVG(Q.final_price),
     MAX(max_bid_idx) AS idx
 FROM (
-    SELECT MAX(B.price) AS final, A.category, MAX(B.idx) as max_bid_idx
+    SELECT 
+        A.id as auction_id, 
+        A.category as category_id, 
+        MAX(B.price) AS final_price, 
+        MAX(B.idx) as max_bid_idx
     FROM auction A, bid B
     WHERE A.id = B.auction AND B.`date_time` BETWEEN A.`date_time` AND A.expires
     GROUP BY A.id, A.category
 ) Q
-GROUP BY Q.category;
+GROUP BY Q.category_id;
