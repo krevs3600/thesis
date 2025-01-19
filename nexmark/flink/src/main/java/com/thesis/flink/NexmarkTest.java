32c72a29c6c8156e64e5ef81c76cd8a9d59cabb3
Carlo <krevs3600@gmail.com>
2025-01-19 10:40:27 +0100

    full table api processing and code cleaning

--- nexmark/flink/src/main/java/com/thesis/flink/NexmarkTest.java
+++ nexmark/flink/src/main/java/com/thesis/flink/NexmarkTest.java
@@ -5,22 +5,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.time.Duration;
-import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Collectors;
 
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.api.java.tuple.Tuple4;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.table.api.DataTypes;
+import org.apache.flink.table.api.EnvironmentSettings;
 import org.apache.flink.table.api.Schema;
 import org.apache.flink.table.api.TableEnvironment;
-import org.apache.flink.table.api.TableResult;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 
 import com.thesis.flink.nexmark.Auction;
@@ -35,15 +30,27 @@ public class NexmarkTest {
     final StreamExecutionEnvironment env; 
     final StreamTableEnvironment tableEnv; 
     final String brokers = "localhost:19092";
-    final DataStream<Person> personStream;
-    final DataStream<Auction> auctionStream;
-    final DataStream<Bid> bidStream;
 
-    
     public NexmarkTest () {
         this.env = StreamExecutionEnvironment.getExecutionEnvironment();
-        this.tableEnv = StreamTableEnvironment.create(env);
-        
+        EnvironmentSettings settings = EnvironmentSettings.newInstance()
+            .inStreamingMode()
+            .build();
+        this.tableEnv = StreamTableEnvironment.create(env, settings);
+        initTableWithTableApi();
+    }
+
+    private void initTableWithTableApi() {
+        String auction = readSqlFile("sources/auction.sql");
+        String person = readSqlFile("sources/person.sql");
+        String bid = readSqlFile("sources/bid.sql");
+
+        tableEnv.executeSql(auction);
+        tableEnv.executeSql(person);
+        tableEnv.executeSql(bid);
+    }
+
+    private void initTableFromKafkaSource(){
         KafkaSource<Person> personSource = KafkaSource.<Person>builder()
             .setBootstrapServers(brokers)
             .setTopics("person-topic")
@@ -68,13 +75,12 @@ public class NexmarkTest {
             .setValueOnlyDeserializer(new BidDeserializationSchema())
             .build();
 
-        // Create DataStreams/ Define the join logic
-        personStream = env.fromSource(personSource, WatermarkStrategy.noWatermarks(), "Person Stream");
-        auctionStream = env.fromSource(auctionSource, WatermarkStrategy.noWatermarks(), "Auction Stream");
-        bidStream = env.fromSource(bidSource, WatermarkStrategy.noWatermarks(), "Bid Stream");
+        // Create DataStreams
+        DataStream<Person> personStream = env.fromSource(personSource, WatermarkStrategy.noWatermarks(), "Person Stream");
+        DataStream<Auction> auctionStream = env.fromSource(auctionSource, WatermarkStrategy.noWatermarks(), "Auction Stream");
+        DataStream<Bid> bidStream = env.fromSource(bidSource, WatermarkStrategy.noWatermarks(), "Bid Stream");
 
-        tableEnv.createTemporaryView(
-            "bid", 
+        tableEnv.createTemporaryView("bid", 
             bidStream,
             Schema.newBuilder()
                 .column("idx", DataTypes.BIGINT())
@@ -126,8 +132,7 @@ public class NexmarkTest {
                 //.columnByExpression("event_time", "TO_TIMESTAMP_LTZ(date_time, 3)") // Derived TIMESTAMP column
                 .watermark("date_time", "date_time - INTERVAL '5' SECOND")             
                 .build()
-        );
-        
+        ); 
     }
     
     public static String readSqlFile(String resourcePath) {
@@ -167,169 +172,6 @@ public class NexmarkTest {
         tableEnv.executeSql(drop_sink);
         tableEnv.executeSql(kafka_sink);
         tableEnv.executeSql("INSERT INTO kafka_sink " + query);
-    }
-
-    public TableResult sqlQuery2(){
-        return tableEnv.executeSql("SELECT auction, price, event_time FROM bid WHERE MOD(auction, 123) = 0;");
-    }
-
-    public TableResult sqlQuery3(){
-        return tableEnv.executeSql("SELECT" + //
-        "   P.name, P.city, P.state, A.id, P" + //
-        "   FROM" + //
-        "   auction AS A INNER JOIN person AS P on A.seller = P.id" + //
-        "   WHERE" + //
-        "   A.category = 10 and (P.state = 'OR' OR P.state = 'ID' OR P.state = 'CA');");
-    }
-
-    public TableResult sqlQuery4(){
-        return tableEnv.executeSql("SELECT" + //
-        "   Q.category," + //
-        "   AVG(Q.final)" + //
-        "   FROM (" + //
-        "   SELECT MAX(B.price) AS final, A.category" + //
-        "   FROM auction A, bid B" + //
-        "   WHERE A.id = B.auction AND B.date_time BETWEEN A.date_time AND A.expires" + // 
-        "   GROUP BY A.id, A.category" + //
-        "   ) Q" + //
-        "   GROUP BY Q.category;");
-    }
-
-    public TableResult sqlQuery5(){
-        return tableEnv.executeSql(  
-        
-        "SELECT AuctionBids.auction, AuctionBids.num" + //
-        " FROM (" + //
-        "   SELECT" + //
-        "     auction," + //
-        "     count(*) AS num," + //
-        "     window_start AS starttime," + //
-        "     window_end AS endtime" + //
-        "     FROM TABLE(" + //
-        "             HOP(TABLE bid, DESCRIPTOR(event_time), INTERVAL '2' SECOND, INTERVAL '10' SECOND))" + //
-        "     GROUP BY auction, window_start, window_end" + //
-        " ) AS AuctionBids" + //
-        " JOIN (" + //
-        "   SELECT" + //
-        "     max(CountBids.num) AS maxn," + //
-        "     CountBids.starttime," + //
-        "     CountBids.endtime" + //
-        "   FROM (" + //
-        "     SELECT" + //
-        "       count(*) AS num," + //
-        "       window_start AS starttime," + //
-        "       window_end AS endtime" + //
-        "     FROM TABLE(" + //
-        "                HOP(TABLE bid, DESCRIPTOR(event_time), INTERVAL '2' SECOND, INTERVAL '10' SECOND))" + //
-        "     GROUP BY auction, window_start, window_end" + //
-        "     ) AS CountBids" + //
-        "   GROUP BY CountBids.starttime, CountBids.endtime" + //
-        " ) AS MaxBids" + //
-        " ON AuctionBids.starttime = MaxBids.starttime AND" + //
-        "    AuctionBids.endtime = MaxBids.endtime AND" + //
-        "    AuctionBids.num >= MaxBids.maxn;");
-    }
-
-    // not supported by flink
-    public TableResult sqlQuery6(){
-        return tableEnv.executeSql("SELECT " + //
-        "    Q.seller, " + //
-        "    AVG(Q.price) OVER (" + //
-        "        PARTITION BY Q.seller " + //
-        "        ORDER BY Q.date_time " + //
-        "        ROWS BETWEEN 10 PRECEDING AND CURRENT ROW" + //
-        "    ) AS avg_price" + //
-        "FROM (" + //
-        "    SELECT * " + //
-        "    FROM (" + //
-        "        SELECT " + //
-        "            A.id, " + //
-        "            A.seller, " + //
-        "            B.price, " + //
-        "            B.date_time, " + //
-        "            ROW_NUMBER() OVER (" + //
-        "                PARTITION BY A.id, A.seller " + //
-        "                ORDER BY B.price DESC" + //
-        "            ) AS rownum" + //
-        "        FROM " + //
-        "            auction AS A" + //
-        "        INNER JOIN " + //
-        "            bid AS B" + //
-        "        ON " + //
-        "            A.id = B.auction " + //
-        "        WHERE " + //
-        "            B.date_time BETWEEN A.date_time AND A.expires" + //
-        "    ) AS SubQ" + //
-        "    WHERE rownum <= 1" + //
-        ") AS Q;");
-    }
-
-    public TableResult sqlQuery7(){
-        return tableEnv.executeSql("SELECT B.auction, B.price, B.bidder, B.event_time, B.extra " + //
-            "FROM bid B " + //
-            "JOIN (" + //
-            "  SELECT MAX(price) AS maxprice, window_end as date_time" + //
-            "  FROM TABLE(" + //
-            "          TUMBLE(TABLE bid, DESCRIPTOR(event_time), INTERVAL '10' SECOND))" + //
-            "  GROUP BY window_start, window_end" + //
-            ") B1 " + //
-            "ON B.price = B1.maxprice " + //
-            "WHERE TO_TIMESTAMP_LTZ(B.date_time,3) BETWEEN B1.date_time  - INTERVAL '10' SECOND AND B1.date_time;");
-    }
-
-    public TableResult sqlQuery8(){
-        return tableEnv.executeSql("SELECT P.id, P.name, P.starttime" + //
-            "FROM (" + //
-            "  SELECT id, name," + //
-            "        window_start AS starttime," + //
-            "        window_end AS endtime" + //
-            "  FROM TABLE(" + //
-            "            TUMBLE(TABLE person, DESCRIPTOR(date_time), INTERVAL '10' SECOND))" + //
-            "  GROUP BY id, name, window_start, window_end" + //
-            ") P" + //
-            "JOIN (" + //
-            "  SELECT seller," + //
-            "        window_start AS starttime," + //
-            "        window_end AS endtime" + //
-            "  FROM TABLE(" + //
-            "        TUMBLE(TABLE auction, DESCRIPTOR(date_time), INTERVAL '10' SECOND))" + //
-            "  GROUP BY seller, window_start, window_end" + //
-            ") A" + //
-            "ON P.id = A.seller AND P.starttime = A.starttime AND P.endtime = A.endtime;");
-    }
-
-
-    public void query1(){
-        bidStream.
-            map(bid -> bid.price *= 0.908);
-    }
-
-   
-
-    public void query2(){
-        bidStream.
-            filter(bid -> {
-                List<Integer> auctionsFilter = Arrays.asList(1020, 2001, 2019, 2087);
-                return auctionsFilter.contains((int) bid.auction);
-            });
-    }
-
-    public void query3(){
-        auctionStream
-            .join(personStream)
-            .where((Auction auction) -> auction.seller)
-            .equalTo((Person person) -> person.id)
-            .window(TumblingEventTimeWindows.of(Duration.ofSeconds(10)))
-            .apply((auction, person) -> { 
-            
-                if (auction.category == 10 && 
-                    (person.state.equals("OR") || person.state.equals("ID") || person.state.equals("CA"))) {
-                        return new Tuple4<>(person.name, person.city, person.state, auction.id); 
-                }
-                return null;
-            })
-            .filter(result -> result != null)
-            .print(); 
     }
 
     public TableEnvironment getTableEnvironment (){
